2a83f48f685be6ec3e319cb91b20a49854f65a85
Cater Tsai <caterokusai@gmail.com>
2018-10-12 16:45:26 +0800

    remove bar

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,13 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1920,1080,OF_WINDOW);			// <-------- setup the GL context
+	//ofSetupOpenGL(1920,1080,OF_WINDOW);			// <-------- setup the GL context
+	ofGLFWWindowSettings settings;
+	settings.width = 1920;
+	settings.height = 1080;
+	settings.decorated = false;
+	settings.windowMode = OF_WINDOW;
+	ofCreateWindow(settings);
 	//ofGLWindowSettings settings;
 	//settings.setGLVersion(3, 2);
 	//ofCreateWindow(settings);
